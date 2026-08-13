f044946bb19054b5d2b6c73d79a1a8b77d75d8f4
mohammed1685shahal-cyber <mohammed1685shahal@gmail.com>
2026-08-13 22:13:42 +0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+console.log("Rainfall game loaded!");
+
+function createAccount() {
+    alert("Create Account system coming next!");
+}
+
+function login() {
+    alert("Login system coming next!");
+}
